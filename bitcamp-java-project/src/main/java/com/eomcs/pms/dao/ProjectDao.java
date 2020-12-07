45652d580181b1959136d164b2b34bcf2bd9920e
ryu seunghee <ryuseunghee.dev@gmail.com>
2020-12-07 18:44:34 +0900

    docs 201207

--- bitcamp-java-project/src/main/java/com/eomcs/pms/dao/ProjectDao.java
+++ bitcamp-java-project/src/main/java/com/eomcs/pms/dao/ProjectDao.java
@@ -7,11 +7,14 @@ import com.eomcs.pms.domain.Project;
 public interface ProjectDao {
   int insert(Project project) throws Exception;
   int delete(int no) throws Exception;
+  int inactive(int no) throws Exception;
   Project findByNo(int no) throws Exception;
   List<Project> findAll(String keyword) throws Exception;
   List<Project> findByDetailKeyword(Map<String,Object> keywords) throws Exception;
   int update(Project project) throws Exception;
 
-  int deleteMembers(int PorjectNo) throws Exception;
+  int deleteMembers(int projectNo) throws Exception;
   int insertMembers(Project project) throws Exception;
+  int updateInactiveMembers(Project project) throws Exception;
+  int updateActiveMembers(Project project) throws Exception;
 }
